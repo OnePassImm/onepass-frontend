ff304c9a238d77461d55eb752a54dff8933d1ea2
huypham501 <pham.giahuy.9041@gmail.com>
2023-12-02 11:18:41 +0700

    Update slide image and ajust approach style.

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -10,7 +12,7 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const Banner = () => {
 	return (
-		<section className="banner h-[85vh]">
+		<section className="banner aspect-[427/240]">
 			<Swiper
 				spaceBetween={30}
 				centeredSlides={true}
@@ -26,24 +28,53 @@ const Banner = () => {
 				modules={[Autoplay, Pagination, Navigation]}
 				className="swiper h-full w-full">
 				<SwiperSlide className="justify-center text-center items-center">
-					<img
-						src={"assets/programGroups/dinhcu.jpg"}
-						alt={"dinhcu.jpg"}
-						className="object-cover h-full w-full block"
-					/>
+					<Link href="">
+						<Image
+							src="/assets/slide-banner/slide-1.jpg"
+							alt="slide-1.jpg"
+							className="object-contain h-full w-full block bg-lightBlue"
+							fill
+						/>
+					</Link>
 				</SwiperSlide>
 				<SwiperSlide className="justify-center text-center items-center">
-					<img
-						src={"assets/programGroups/duhoc.jpg"}
-						alt={"duhoc.jpg"}
-						className="object-cover h-full w-full block"
+					<Image
+						src="/assets/slide-banner/slide-2.jpg"
+						alt="slide-2.jpg"
+						className="object-contain h-full w-full block bg-lightBlue"
+						fill
 					/>
 				</SwiperSlide>
 				<SwiperSlide className="flex justify-center text-center items-center">
-					<img
-						src={"assets/programGroups/dulich.jpg"}
-						alt={"dulich.jpg"}
-						className="object-cover h-full w-full block"
+					<Image
+						src="/assets/slide-banner/slide-3.jpg"
+						alt="slide-3.jpg"
+						className="object-contain h-full w-full block bg-lightBlue"
+						fill
+					/>
+				</SwiperSlide>
+				<SwiperSlide className="flex justify-center text-center items-center">
+					<Image
+						src="/assets/slide-banner/slide-4.jpg"
+						alt="slide-4.jpg"
+						className="object-contain h-full w-full block bg-lightBlue"
+						fill
+					/>
+				</SwiperSlide>
+				<SwiperSlide className="flex justify-center text-center items-center">
+					<Image
+						src="/assets/slide-banner/slide-5.jpg"
+						alt="slide-5.jpg"
+						className="object-contain h-full w-full block bg-lightBlue"
+						fill
+					/>
+				</SwiperSlide>
+				<SwiperSlide className="flex justify-center text-center items-center">
+					<Image
+						src="/assets/slide-banner/slide-6.jpg"
+						alt="slide-6.jpg"
+						className="object-contain h-full w-full block bg-lightBlue"
+						fill
 					/>
 				</SwiperSlide>
 			</Swiper>
